0e387a818d0461355de47bba01bf65223a39b240
Mario Bustos <mario.bustos@phs.mx>
2026-02-20 12:13:38 -0600

    fix: extract constants, noreferrer, remove redundant styles, aria-hidden SVGs

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const RELEASES_URL = 'https://github.com/mariobustosjmz/claudeDock/releases/latest';
+export const STRIPE_URL = 'https://buy.stripe.com/PLACEHOLDER';
+export const GITHUB_URL = 'https://github.com/mariobustosjmz/claudeDock';
+export const NIRBY_URL = 'https://nir.by/';
